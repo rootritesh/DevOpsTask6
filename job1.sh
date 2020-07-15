79652f282e3ae9e47382b58da5617f7600b8ade0
rootritesh <57373623+rootritesh@users.noreply.github.com>
2020-07-15 12:54:16 +0530

    1st commmit

---
+++ job1.sh
@@ -0,0 +1,25 @@
+if ls | grep *.php
+then
+if kubectl get deployment | grep myphp
+then
+echo "php container already running"
+POD=$(kubectl get pods -l app=php -o jsonpath="{.items[*].metadata.name}")
+sudo kubectl cp *.php $POD:/var/www/html
+else
+sudo kubectl create -f depphp.yaml
+sleep 10
+POD=$(kubectl get pods -l app=php -o jsonpath="{.items[*].metadata.name}")
+sudo kubectl cp *.php $POD:/var/www/html
+fi
+else
+if kubectl get deployment | grep html
+then
+echo "html conatiner already running"
+POD=$(kubectl get pods -l app=html -o jsonpath="{.items[*].metadata.name}")
+sudo kubectl cp *.php $POD:/var/www/html
+else
+sudo kubectl create -f dephtml.yaml
+POD=$(kubectl get pods -l app=html -o jsonpath="{.items[*].metadata.name}")
+sudo kubectl cp *.php $POD:/usr/local/apache2/htdocs
+fi
+fi
