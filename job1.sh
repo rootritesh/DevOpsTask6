ba3e41f2f7000a395b0bee53f8fa8f476e056e2b
rootritesh <57373623+rootritesh@users.noreply.github.com>
2020-07-19 17:16:25 +0530

    sudoremoved

--- job1.sh
+++ job1.sh
@@ -4,23 +4,23 @@ if kubectl get deployment | grep myphp
 then
 echo "php container already running"
 POD=$(kubectl get pods -l app=php -o jsonpath="{.items[*].metadata.name}")
-sudo kubectl cp *.php $POD:/var/www/html
+kubectl cp *.php $POD:/var/www/html
 else
 sudo kubectl create -f depphp.yaml
 sleep 10
 POD=$(kubectl get pods -l app=php -o jsonpath="{.items[*].metadata.name}")
-sudo kubectl cp *.php $POD:/var/www/html
+kubectl cp *.php $POD:/var/www/html
 fi
 else
 if kubectl get deployment | grep html
 then
 echo "html conatiner already running"
 POD=$(kubectl get pods -l app=html -o jsonpath="{.items[*].metadata.name}")
-sudo kubectl cp *.html $POD:/var/www/html
+kubectl cp *.html $POD:/var/www/html
 else
 sudo kubectl create -f dephtml.yaml
 sleep 10
 POD=$(kubectl get pods -l app=html -o jsonpath="{.items[*].metadata.name}")
-sudo kubectl cp *.html $POD:/usr/local/apache2/htdocs
+kubectl cp *.html $POD:/usr/local/apache2/htdocs
 fi
 fi
